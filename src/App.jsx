e0ce483aa91daed130e1bc62e654c9582a88eb88
junaidkhatri56 <developerjunaidkhatri@gmail.com>
2024-10-02 02:44:27 +0500

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,46 @@
 import { useState } from 'react';
 import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0);
+// function App() {
+//   const [count, setCount] = useState(0);
   
-  function addCounter() {
-    setCount(count + 1);
-  }
+//   function addCounter() {
+//     setCount(count + 1);
+//   }
 
-  function lessCounter() {
-    if (count > 0) {
-      setCount(count - 1);
-    }
-  }
+//   function lessCounter() {
+//     if (count > 0) {
+//       setCount(count - 1);
+//     }
+//   }
 
+//   return (
+//     <div className="app-container">
+//       <div className="counter-box">
+//         <h1 className="counter-text">Hello World {count}</h1>
+//         <div className="button-group">
+//           <button className="button add-button" onClick={addCounter}>
+//             Add Counter
+//           </button>
+//           <button className="button less-button" onClick={lessCounter}>
+//             Less Counter
+//           </button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+function App(){
+  const input = useref()
+  const [title , setTitle] = useState([])
   return (
-    <div className="app-container">
-      <div className="counter-box">
-        <h1 className="counter-text">Hello World {count}</h1>
-        <div className="button-group">
-          <button className="button add-button" onClick={addCounter}>
-            Add Counter
-          </button>
-          <button className="button less-button" onClick={lessCounter}>
-            Less Counter
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+    <>
+      <input type="text" placeholder='enter your title' ref={input} />
+      <button onClick={()=> setTitle(input.current.value)}> Add </button>  
+    </>
+  )
 }
 
 export default App;
